4faa8d2620c1fb9db36c9578b7d3fa91b6ebb61c
Peter Liang <peter@userdeMacBook-Pro.local>
2015-03-18 22:38:55 +0800

    Project built.

---
+++ main/src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * Created by peter on 3/18/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        CodeHere();
+        long end = System.currentTimeMillis();
+        System.out.println("Time elapsed: " + (end - start) + "ms");
+    }
+
+    private static void CodeHere() {
+    }
+}
