b450e60653dc26fa4506b1ba3621f03d020814aa
Peter Liang <peter@userdeMacBook-Pro.local>
2015-03-19 16:55:20 +0800

    Finished kata Counting power sets. http://www.codewars.com/kata/54381f0b6f032f933c000108/solutions/java

---
+++ main/src/Powers.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+/**
+ * Created by peter on 3/19/15.
+ * Solution of Counting power sets.
+ * http://www.codewars.com/kata/54381f0b6f032f933c000108/train/java
+ */
+public class Powers {
+    public static BigInteger powers(int[] list) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 0; i < list.length; i++) {
+            result = result.add(result);
+        }
+        return result;
+    }
+}
