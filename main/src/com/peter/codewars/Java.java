2596ad0ac92915086354208ce70e55773d80ecea
Peter Liang <peter@userdeMacBook-Pro.local>
2015-03-21 09:12:00 +0800

    Solved kata 'You Can't Code Under Pressure #1'. http://www.codewars.com/kata/53ee5429ba190077850011d4/solutions/java

---
+++ main/src/com/peter/codewars/Java.java
@@ -0,0 +1,12 @@
+package com.peter.codewars;
+
+/**
+ * Created by peter on 3/21/15.
+ * Solution of You Can't Code Under Pressure #1.
+ * http://www.codewars.com/kata/53ee5429ba190077850011d4/train/java
+ */
+public class Java {
+    public static int doubleInteger(int i) {
+        return i << 1;
+    }
+}
