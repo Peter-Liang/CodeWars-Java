0ede4e65f55c44f0e4be5f1c239942cd4ce55954
Peter Liang <peter_liang@outlook.com>
2015-07-01 09:34:41 +0800

    Solved the kata 'Josephus Survivor'. http://www.codewars.com/kata/555624b601231dc7a400017a/solutions/java

---
+++ main/src/com/peter/codewars/JosephusSurvivor.java
@@ -0,0 +1,22 @@
+package com.peter.codewars;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Josephus Survivor
+ * http://www.codewars.com/kata/555624b601231dc7a400017a/train/java
+ */
+public class JosephusSurvivor {
+    public static int josephusSurvivor(final int n, final int k) {
+        List<Integer> people = IntStream.rangeClosed(1, n).boxed().collect(Collectors.toList());
+        int index = 0;
+        while (people.size() > 1) {
+            index = (index + (k - 1)) % people.size();
+            people.remove(index);
+        }
+
+        return people.get(0);
+    }
+}
