717c65bfe8247f8da3ffac8ce15a33700ecabec0
Peter Liang <peter_liang@outlook.com>
2015-07-11 18:54:15 +0800

    Solved the kata 'Xbonacci'. http://www.codewars.com/kata/556deca17c58da83c00002db/solutions/java

---
+++ main/src/com/peter/codewars/Xbonacci.java
@@ -0,0 +1,20 @@
+package com.peter.codewars;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Tribonacci Sequence
+ * http://www.codewars.com/kata/556deca17c58da83c00002db/train/java
+ */
+public class Xbonacci {
+    public double[] tribonacci(double[] s, int n) {
+        List<Double> result = Arrays.stream(s).boxed().collect(Collectors.toList());
+        for (int i = result.size() - 1; i < n - 1; i++) {
+            result.add(result.get(i - 2) + result.get(i - 1) + result.get(i));
+        }
+
+        return result.stream().limit(n).mapToDouble(Double::doubleValue).toArray();
+    }
+}
---
+++ main/src/com/peter/codewarstest/XbonacciTest.java
@@ -0,0 +1,24 @@
+package com.peter.codewarstest;
+
+import com.peter.codewars.Xbonacci;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class XbonacciTest {
+
+    private Xbonacci _xbonacci;
+
+    @Before
+    public void setUp() {
+        _xbonacci = new Xbonacci();
+    }
+
+    @Test
+    public void basicTest() {
+        assertArrayEquals(new double[]{1, 1, 1, 3, 5, 9, 17, 31, 57, 105}, _xbonacci.tribonacci(new double[]{1, 1, 1}, 10), 0);
+        assertArrayEquals(new double[]{0, 0, 1, 1, 2, 4, 7, 13, 24, 44}, _xbonacci.tribonacci(new double[]{0, 0, 1}, 10), 0);
+        assertArrayEquals(new double[]{0, 1, 1, 2, 4, 7, 13, 24, 44, 81}, _xbonacci.tribonacci(new double[]{0, 1, 1}, 10), 0);
+    }
+}
