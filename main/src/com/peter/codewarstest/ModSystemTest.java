8a0ae014f79f58ae1fd24ef33ff401c1365f7f61
Peter Liang <peter_liang@outlook.com>
2015-07-12 22:26:27 +0800

    Finally solved the kata 'Moduli number system'. http://www.codewars.com/kata/54db15b003e88a6a480000b9/solutions/java

---
+++ main/src/com/peter/codewarstest/ModSystemTest.java
@@ -0,0 +1,25 @@
+package com.peter.codewarstest;
+
+import com.peter.codewars.ModSystem;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ModSystemTest {
+
+    @Test
+    public void basicTest() {
+        assertEquals("-3--5--2--1-", ModSystem.fromNb2Str(187, new int[]{8, 7, 5, 3}));
+    }
+
+    @Test
+    public void notCoprimeNotApplicable() {
+        assertEquals("Not applicable", ModSystem.fromNb2Str(6, new int[]{2, 3, 4}));
+        assertEquals("Not applicable", ModSystem.fromNb2Str(15, new int[]{8, 6, 5}));
+    }
+
+    @Test
+    public void notBiggerNotApplicable() {
+        assertEquals("Not applicable", ModSystem.fromNb2Str(7, new int[]{2, 3}));
+    }
+}
