23edba6d18fa5f5ad25f617ce51cdd3ba168808b
Peter Liang <peter_liang@outlook.com>
2015-07-13 10:11:25 +0800

    Removed redundant code.

--- main/src/com/peter/codewars/MorseCodeDecoder.java
+++ main/src/com/peter/codewars/MorseCodeDecoder.java
@@ -13,18 +13,6 @@ public class MorseCodeDecoder {
     private static Map<String, String> MorseCode = new HashMap<>();
 
     public static String decode(String morseCode) {
-        morseCode = morseCode.trim();
-        String[] words = morseCode.split("   ");
-        StringBuilder result = new StringBuilder();
-        for (String word : words) {
-            String[] charater = word.split(" ");
-            Arrays.stream(charater).forEach(c -> result.append(MorseCode.get(c)));
-            result.append(" ");
-        }
-        if (result.length() > 0) {
-            result.deleteCharAt(result.length() - 1);
-        }
-//        return result.toString();
         return Arrays.stream(morseCode.trim().split("\\s{3}"))
                 .map(w -> Arrays.stream(w.split("\\s"))
                         .map(MorseCode::get)
