f6a49e43ee08174fb21722209a0adcdb35fd3915
Peter Liang <peter_liang@outlook.com>
2015-07-14 15:44:43 +0800

    Solution of 'Decode the Morse code, advanced'.

--- main/src/com/peter/codewars/MorseCodeDecoder.java
+++ main/src/com/peter/codewars/MorseCodeDecoder.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
 /**
  * Decode the Morse code
  * http://www.codewars.com/kata/54b724efac3d5402db00065e/train/java
+ * <p/>
+ * Decode the Morse code, advanced
+ * http://www.codewars.com/kata/54b72c16cd7f5154e9000457/train/java
  */
 public class MorseCodeDecoder {
     private static Map<String, String> MorseCode = new HashMap<>();
@@ -18,5 +21,30 @@ public class MorseCodeDecoder {
                         .map(MorseCode::get)
                         .collect(Collectors.joining()))
                 .collect(Collectors.joining(" "));
+    }
+
+    public static String decodeBits(String bits) {
+        int times = Arrays.stream(bits.split("0"))
+                .mapToInt(String::length)
+                .filter(i -> i != 0)
+                .min()
+                .getAsInt();
+        bits = bits.replaceAll("(.){" + times + "}", "$1");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < bits.length() - 1; i += 2) {
+            String bit = bits.substring(i, i + 2);
+            switch (bit) {
+                case "00":
+                    result.append(" ");
+                    break;
+                case "10":
+                    result.append(".");
+                    break;
+                case "11":
+                    result.append("-");
+                    break;
+            }
+        }
+        return result.toString();
     }
 }
