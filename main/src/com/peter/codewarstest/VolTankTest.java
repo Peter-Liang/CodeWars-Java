377f2c870921fa0d26cf126d86c0a80b4d734b94
Peter Liang <peter_liang@outlook.com>
2016-07-29 23:26:33 +0800

    Solved the 6 kyu kata 'Tank Truck'. https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/solutions/java

---
+++ main/src/com/peter/codewarstest/VolTankTest.java
@@ -0,0 +1,17 @@
+package com.peter.codewarstest;
+
+import com.peter.codewars.VolTank;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class VolTankTest {
+
+    @Test
+    public void test0() {
+        System.out.println("Fixed Tests");
+        assertEquals(2940, VolTank.tankVol(5, 7, 3848));
+        assertEquals(907, VolTank.tankVol(2, 7, 3848));
+    }
+}
